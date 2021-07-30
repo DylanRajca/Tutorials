ab0c39201f6d598c4355ed75e59273291c2d600d
Dylan Rajca <dyrajca93@gmail.com>
2021-07-30 16:59:16 -0700

    Classes

---
+++ Python/classMethods.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    def __init__(self, c, l, w):
+        self.color = c
+        self.length = l
+        self.width = w
+
+
+myRect1 = Rectangle('red', 2, 1)
+myRect2 = Rectangle('blue', 4, 2)
